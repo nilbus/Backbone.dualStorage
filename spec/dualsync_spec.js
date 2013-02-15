80aacc28b90c9ed47c8403c55c1369c3bd65b6ec
Edward Anderson <nilbus@nilbus.com>
2013-02-15 17:19:24 -0500

    Convert spyOnLocalSync
    The huge spyOnLocalSync used to be necessary in the node coffee sandbox,
    but we can use a normal spy now.

--- spec/dualsync_spec.js
+++ spec/dualsync_spec.js
@@ -17,21 +17,11 @@
     return delete model.remote;
   });
 
-  spyOnLocalsync = function(spec) {
-    var coffee;
-    coffee = require('coffee-script');
-    runs(function() {
-      return coffee["eval"]("window.oldLocalsync = window.localsync; window.localsync = jasmine.createSpy('localsync').andCallFake((method, model, options) -> (options.success?()) unless options.ignoreCallbacks)", {
-        sandbox: window
-      });
-    });
-    runs(function() {
-      return spec(window.localsync);
-    });
-    return runs(function() {
-      return coffee["eval"]("window.localsync = window.oldLocalsync", {
-        sandbox: window
-      });
+  spyOnLocalsync = function() {
+    return spyOn(window, 'localsync').andCallFake(function(method, model, options) {
+      if (!options.ignoreCallbacks) {
+        return typeof options.success === "function" ? options.success() : void 0;
+      }
     });
   };
 
@@ -39,109 +29,105 @@
     describe('dual tier storage', function() {
       describe('create', function() {
         return it('delegates to both localsync and onlinesync', function() {
-          return spyOnLocalsync(function() {
-            var ready;
-            ready = false;
-            runs(function() {
-              return window.dualsync('create', model, {
-                success: (function() {
-                  return ready = true;
-                })
-              });
+          var ready;
+          spyOnLocalsync();
+          ready = false;
+          runs(function() {
+            return window.dualsync('create', model, {
+              success: (function() {
+                return ready = true;
+              })
             });
-            waitsFor((function() {
-              return ready;
-            }), "The success callback should have been called", 100);
-            return runs(function() {
-              expect(window.onlineSync).toHaveBeenCalled();
-              expect(window.onlineSync.calls[0].args[0]).toEqual('create');
-              expect(window.localsync).toHaveBeenCalled();
-              return expect(window.localsync.calls[0].args[0]).toEqual('create');
-            });
+          });
+          waitsFor((function() {
+            return ready;
+          }), "The success callback should have been called", 100);
+          return runs(function() {
+            expect(window.onlineSync).toHaveBeenCalled();
+            expect(window.onlineSync.calls[0].args[0]).toEqual('create');
+            expect(window.localsync).toHaveBeenCalled();
+            return expect(window.localsync.calls[0].args[0]).toEqual('create');
           });
         });
       });
       describe('read', function() {
         return it('delegates to both localsync and onlinesync', function() {
-          return spyOnLocalsync(function() {
-            var ready;
-            ready = false;
-            runs(function() {
-              return window.dualsync('read', model, {
-                success: (function() {
-                  return ready = true;
-                })
-              });
+          var ready;
+          spyOnLocalsync();
+          ready = false;
+          runs(function() {
+            return window.dualsync('read', model, {
+              success: (function() {
+                return ready = true;
+              })
             });
-            waitsFor((function() {
-              return ready;
-            }), "The success callback should have been called", 100);
-            return runs(function() {
-              expect(window.onlineSync).toHaveBeenCalled();
-              expect(_(window.onlineSync.calls).any(function(call) {
-                return call.args[0] === 'read';
-              }));
-              expect(window.localsync).toHaveBeenCalled();
-              return expect(_(window.localsync.calls).any(function(call) {
-                return call.args[0] === 'read';
-              }));
-            });
+          });
+          waitsFor((function() {
+            return ready;
+          }), "The success callback should have been called", 100);
+          return runs(function() {
+            expect(window.onlineSync).toHaveBeenCalled();
+            expect(_(window.onlineSync.calls).any(function(call) {
+              return call.args[0] === 'read';
+            }));
+            expect(window.localsync).toHaveBeenCalled();
+            return expect(_(window.localsync.calls).any(function(call) {
+              return call.args[0] === 'read';
+            }));
           });
         });
       });
       describe('update', function() {
         return it('delegates to both localsync and onlinesync', function() {
-          return spyOnLocalsync(function() {
-            var ready;
-            ready = false;
-            runs(function() {
-              return window.dualsync('update', model, {
-                success: (function() {
-                  return ready = true;
-                })
-              });
+          var ready;
+          spyOnLocalsync();
+          ready = false;
+          runs(function() {
+            return window.dualsync('update', model, {
+              success: (function() {
+                return ready = true;
+              })
             });
-            waitsFor((function() {
-              return ready;
-            }), "The success callback should have been called", 100);
-            return runs(function() {
-              expect(window.onlineSync).toHaveBeenCalled();
-              expect(_(window.onlineSync.calls).any(function(call) {
-                return call.args[0] === 'update';
-              }));
-              expect(window.localsync).toHaveBeenCalled();
-              return expect(_(window.localsync.calls).any(function(call) {
-                return call.args[0] === 'update';
-              }));
-            });
+          });
+          waitsFor((function() {
+            return ready;
+          }), "The success callback should have been called", 100);
+          return runs(function() {
+            expect(window.onlineSync).toHaveBeenCalled();
+            expect(_(window.onlineSync.calls).any(function(call) {
+              return call.args[0] === 'update';
+            }));
+            expect(window.localsync).toHaveBeenCalled();
+            return expect(_(window.localsync.calls).any(function(call) {
+              return call.args[0] === 'update';
+            }));
           });
         });
       });
       return describe('delete', function() {
         return it('delegates to both localsync and onlinesync', function() {
-          return spyOnLocalsync(function() {
-            var ready;
-            ready = false;
-            runs(function() {
-              return window.dualsync('delete', model, {
-                success: (function() {
-                  return ready = true;
-                })
-              });
+          var ready;
+          spyOnLocalsync();
+          ready = false;
+          runs(function() {
+            return window.dualsync('delete', model, {
+              success: (function() {
+                return ready = true;
+              })
             });
-            waitsFor((function() {
-              return ready;
-            }), "The success callback should have been called", 100);
-            return runs(function() {
-              expect(window.onlineSync).toHaveBeenCalled();
-              expect(_(window.onlineSync.calls).any(function(call) {
-                return call.args[0] === 'delete';
-              }));
-              expect(window.localsync).toHaveBeenCalled();
-              return expect(_(window.localsync.calls).any(function(call) {
-                return call.args[0] === 'delete';
-              }));
-            });
+          });
+          waitsFor((function() {
+            return ready;
+          }), "The success callback should have been called", 100);
+          return runs(function() {
+            expect(window.onlineSync).toHaveBeenCalled();
+            expect(_(window.onlineSync.calls).any(function(call) {
+              return call.args[0] === 'delete';
+            }));
+            expect(window.localsync).toHaveBeenCalled();
+            return expect(_(window.localsync.calls).any(function(call) {
+              return call.args[0] === 'delete';
+            }));
           });
         });
       });
@@ -188,25 +174,24 @@
     });
     describe('respects the local only attribute on models', function() {
       it('delegates for local models', function() {
-        return spyOnLocalsync(function() {
-          var ready;
-          ready = false;
-          runs(function() {
-            model.local = true;
-            window.onlineSync.reset();
-            return window.dualsync('update', model, {
-              success: (function() {
-                return ready = true;
-              })
-            });
+        var ready;
+        spyOnLocalsync();
+        ready = false;
+        runs(function() {
+          model.local = true;
+          window.onlineSync.reset();
+          return window.dualsync('update', model, {
+            success: (function() {
+              return ready = true;
+            })
           });
-          waitsFor((function() {
-            return ready;
-          }), "The success callback should have been called", 100);
-          return runs(function() {
-            expect(window.localsync).toHaveBeenCalled();
-            return expect(window.localsync.calls[0].args[0]).toEqual('update');
-          });
+        });
+        waitsFor((function() {
+          return ready;
+        }), "The success callback should have been called", 100);
+        return runs(function() {
+          expect(window.localsync).toHaveBeenCalled();
+          return expect(window.localsync.calls[0].args[0]).toEqual('update');
         });
       });
       return it('delegates for local collections', function() {
@@ -252,46 +237,45 @@
 
   describe('offline storage', function() {
     return it('marks records dirty when options.remote is false, except if the model/collection is marked as local', function() {
-      return spyOnLocalsync(function() {
-        var ready;
-        ready = void 0;
-        runs(function() {
-          ready = false;
-          collection.local = true;
-          return window.dualsync('update', model, {
-            success: (function() {
-              return ready = true;
-            }),
-            remote: false
-          });
+      var ready;
+      spyOnLocalsync();
+      ready = void 0;
+      runs(function() {
+        ready = false;
+        collection.local = true;
+        return window.dualsync('update', model, {
+          success: (function() {
+            return ready = true;
+          }),
+          remote: false
         });
-        waitsFor((function() {
-          return ready;
-        }), "The success callback should have been called", 100);
-        runs(function() {
-          expect(window.localsync).toHaveBeenCalled();
-          expect(window.localsync.calls.length).toEqual(1);
-          return expect(window.localsync.calls[0].args[2].dirty).toBeFalsy();
+      });
+      waitsFor((function() {
+        return ready;
+      }), "The success callback should have been called", 100);
+      runs(function() {
+        expect(window.localsync).toHaveBeenCalled();
+        expect(window.localsync.calls.length).toEqual(1);
+        return expect(window.localsync.calls[0].args[2].dirty).toBeFalsy();
+      });
+      runs(function() {
+        window.localsync.reset();
+        ready = false;
+        collection.local = false;
+        return window.dualsync('update', model, {
+          success: (function() {
+            return ready = true;
+          }),
+          remote: false
         });
-        runs(function() {
-          window.localsync.reset();
-          ready = false;
-          collection.local = false;
-          return window.dualsync('update', model, {
-            success: (function() {
-              return ready = true;
-            }),
-            remote: false
-          });
-        });
-        waitsFor((function() {
-          return ready;
-        }), "The success callback should have been called", 100);
-        return runs(function() {
-          expect(window.localsync).toHaveBeenCalled();
-          expect(window.localsync.calls.length).toEqual(1);
-          return expect(window.localsync.calls[0].args[2].dirty).toBeTruthy();
-        });
+      });
+      waitsFor((function() {
+        return ready;
+      }), "The success callback should have been called", 100);
+      return runs(function() {
+        expect(window.localsync).toHaveBeenCalled();
+        expect(window.localsync.calls.length).toEqual(1);
+        return expect(window.localsync.calls[0].args[2].dirty).toBeTruthy();
       });
     });
   });
