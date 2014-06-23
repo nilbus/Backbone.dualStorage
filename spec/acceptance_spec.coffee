2da2e32cdcfab6da66a324c342df115739e40671
Edward Anderson <nilbus@nilbus.com>
2014-06-22 22:53:21 -0400

    Implement more acceptance tests
    Fix ineffective falsy expectation.

--- spec/acceptance_spec.coffee
+++ spec/acceptance_spec.coffee
@@ -5,7 +5,6 @@ describe 'Backbone.dualStorage', ->
   @timeout 10
 
   beforeEach ->
-    backboneSync.calls = []
     localStorage.clear()
     class Model extends Backbone.Model
       idAttribute: '_id'
@@ -47,7 +46,7 @@ describe 'Backbone.dualStorage', ->
               model.fetch remote: false, success: ->
                 fetchedLocally.resolve()
               fetchedLocally.done ->
-                expect(model.get('pants')).to.be.falsy
+                expect(model.get('pants')).to.be.undefined
                 expect(model.get('shoes')).to.equal 'boots'
                 done()
 
@@ -128,11 +127,11 @@ describe 'Backbone.dualStorage', ->
                 model.fetch remote: false, success: ->
                   fetchedLocally.resolve()
                 fetchedLocally.done ->
-                  expect(model.get('paper')).to.be.falsy
+                  expect(model.get('paper')).to.be.undefined
                   expect(model.get('leather')).to.equal 'belts'
                   done()
 
-      describe 'Collection.destroy', ->
+      describe 'Model.destroy', ->
         it 'removes the locally stored version', (done) ->
           saved = $.Deferred()
           model = new Model _id: 1
@@ -250,3 +249,164 @@ describe 'Backbone.dualStorage', ->
                     expect(collection.length).to.equal 3
                     expect(collection.get(3).get('hair')).to.equal 'chocolate'
                     done()
+
+    describe 'offline operations cached for syncing later', ->
+      describe 'Model.save, Model.fetch', ->
+        it 'creates new records', (done) ->
+          saved = $.Deferred()
+          model = new Model
+          model.save 'paper', 'oragami', errorStatus: 0, success: ->
+            saved.resolve()
+          saved.done ->
+            fetchedLocally = $.Deferred()
+            model = new Model _id: model.id
+            model.fetch errorStatus: 0, success: ->
+              fetchedLocally.resolve()
+            fetchedLocally.done ->
+              expect(model.get('paper')).to.equal 'oragami'
+              done()
+
+        it 'updates records created while offline', (done) ->
+          saved = $.Deferred()
+          model = new Model
+          model.save 'paper', 'oragami', errorStatus: 0, success: ->
+            saved.resolve()
+          saved.done ->
+            updated = $.Deferred()
+            model.save 'paper', 'mâché', errorStatus: 0, success: ->
+              updated.resolve()
+            updated.done ->
+              fetchedLocally = $.Deferred()
+              model = new Model _id: model.id
+              model.fetch errorStatus: 0, success: ->
+                fetchedLocally.resolve()
+              fetchedLocally.done ->
+                expect(model.get('paper')).to.equal 'mâché'
+                done()
+
+      describe 'Collection.fetch', ->
+        it 'loads models that were saved with a common storeName/urlRoot', (done) ->
+          saved1 = $.Deferred()
+          model1 = new Model a: 1
+          model1.save 'paper', 'oragami', errorStatus: 0, success: ->
+            saved1.resolve()
+          saved2 = $.Deferred()
+          model2 = new Model a: 2
+          model2.save 'paper', 'oragami', errorStatus: 0, success: ->
+            saved2.resolve()
+          $.when(saved1, saved2).done ->
+            fetched = $.Deferred()
+            collection = new Collection
+            collection.fetch errorStatus: 0, success: ->
+              fetched.resolve()
+            fetched.done ->
+              expect(collection.size()).to.equal 2
+              expect(collection.get(model1.id).attributes).to.eql model1.attributes
+              expect(collection.get(model2.id).attributes).to.eql model2.attributes
+              done()
+
+      describe 'Model.id', ->
+        it 'obtains a temporary id on new records for use until saved online', (done) ->
+          saved = $.Deferred()
+          model = new Model
+          model.save null, errorStatus: 0, success: ->
+            saved.resolve()
+          saved.done ->
+            expect(model.id.length).to.equal 36
+            done()
+
+  describe 'syncing offline changes when there are dirty or destroyed records', ->
+    beforeEach (done) ->
+      # Save two models in a collection while online.
+      # Then while offline, modify one, and delete the other
+      @collection = new Collection [
+        {_id: 1, name: 'change me'},
+        {_id: 2, name: 'delete me'}
+      ]
+      allSaved = @collection.map (model) ->
+        saved = $.Deferred()
+        model.save null, success: ->
+          saved.resolve()
+        saved
+      allModified = $.when(allSaved...).then =>
+        dirtied = $.Deferred()
+        @collection.get(1).save 'name', 'dirty me', errorStatus: 0, success: ->
+          dirtied.resolve()
+        destroyed = $.Deferred()
+        @collection.get(2).destroy errorStatus: 0, success: ->
+          destroyed.resolve()
+        $.when dirtied, destroyed
+      allModified.done ->
+        done()
+
+    describe 'Model.fetch', ->
+      it 'reads models in dirty collections from local storage until a successful sync', (done) ->
+        fetched = $.Deferred()
+        model = new Model _id: 1
+        model.fetch serverResponse: {_id: 1, name: 'this response is never used'}, success: ->
+          fetched.resolve()
+        fetched.done ->
+          expect(model.get('name')).to.equal 'dirty me'
+          done()
+
+    describe 'Collection.fetch', ->
+      it 'excludes destroyed models when working locally before a sync', (done) ->
+        fetched = $.Deferred()
+        collection = new Collection
+        collection.fetch serverResponse: [{_id: 3, name: 'this response is never used'}], success: ->
+          fetched.resolve()
+        fetched.done ->
+          expect(collection.size()).to.equal 1
+          expect(collection.first().get('name')).to.equal 'dirty me'
+          done()
+
+    describe 'Collection.dirtyModels', ->
+      it 'returns an array of models that have been created or updated while offline', ->
+        expect(@collection.dirtyModels()).to.eql [@collection.get(1)]
+
+    describe 'Collection.destroyedModelIds', ->
+      it 'returns an array of ids for models that have been destroyed while offline', ->
+        expect(@collection.destroyedModelIds()).to.eql ['2']
+
+    # These sync methods are synchronous only in this test environment.
+    # The async branch will provide a promise that we can use to know when it completes.
+    # In the current version, there is no callback.
+
+    describe 'Collection.syncDirty', ->
+      it 'attempts to save online all records that were created/updated while offline', ->
+        backboneSync.reset()
+        @collection.syncDirty()
+        expect(backboneSync.callCount).to.equal 1
+        expect(@collection.dirtyModels()).to.eql []
+
+    describe 'Collection.syncDestroyed', ->
+      it 'attempts to destroy online all records that were destroyed while offline', ->
+        backboneSync.reset()
+        @collection.syncDestroyed()
+        expect(backboneSync.callCount).to.equal 1
+        expect(@collection.destroyedModelIds()).to.eql []
+
+    describe 'Collection.syncDirtyAndDestroyed', ->
+      it 'attempts to sync online all records that were modified while offline', ->
+        backboneSync.reset()
+        @collection.syncDirtyAndDestroyed()
+        expect(backboneSync.callCount).to.equal 2
+        expect(@collection.dirtyModels()).to.eql []
+        expect(@collection.destroyedModelIds()).to.eql []
+
+    describe 'Model.id', ->
+      it 'for new records with a temporary id is replaced by the id returned by the server', (done) ->
+        saved = $.Deferred()
+        model = new Model
+        @collection.add model
+        model.save 'name', 'created while offline', errorStatus: 0, success: ->
+          saved.resolve()
+        saved.done =>
+          expect(model.id.length).to.equal 36
+          backboneSync.reset()
+          @collection.syncDirty()
+          expect(backboneSync.callCount).to.equal 2
+          expect(backboneSync.lastCall.args[0]).to.equal 'create'
+          expect(backboneSync.lastCall.args[1].id).to.be.null
+          expect(backboneSync.lastCall.args[1].get('_id')).to.be.null
+          done()
--- spec/acceptance_spec.js
+++ spec/acceptance_spec.js
@@ -11,7 +11,6 @@
   describe('Backbone.dualStorage', function() {
     this.timeout(10);
     beforeEach(function() {
-      backboneSync.calls = [];
       localStorage.clear();
       Model = (function(_super) {
         __extends(Model, _super);
@@ -42,8 +41,8 @@
 
       })(Backbone.Collection);
     });
-    return describe('basic persistence', function() {
-      return describe('online operations cached for offline use', function() {
+    describe('basic persistence', function() {
+      describe('online operations cached for offline use', function() {
         describe('Model.fetch', function() {
           it('stores the result locally after fetch', function(done) {
             var fetchedOnline, model;
@@ -121,7 +120,7 @@
                   }
                 });
                 return fetchedLocally.done(function() {
-                  expect(model.get('pants')).to.be.falsy;
+                  expect(model.get('pants')).to.be.undefined;
                   expect(model.get('shoes')).to.equal('boots');
                   return done();
                 });
@@ -303,7 +302,7 @@
                     }
                   });
                   return fetchedLocally.done(function() {
-                    expect(model.get('paper')).to.be.falsy;
+                    expect(model.get('paper')).to.be.undefined;
                     expect(model.get('leather')).to.equal('belts');
                     return done();
                   });
@@ -312,7 +311,7 @@
             });
           });
         });
-        describe('Collection.destroy', function() {
+        describe('Model.destroy', function() {
           it('removes the locally stored version', function(done) {
             var model, saved;
             saved = $.Deferred();
@@ -565,6 +564,288 @@
               });
             });
           });
+        });
+      });
+      return describe('offline operations cached for syncing later', function() {
+        describe('Model.save, Model.fetch', function() {
+          it('creates new records', function(done) {
+            var model, saved;
+            saved = $.Deferred();
+            model = new Model;
+            model.save('paper', 'oragami', {
+              errorStatus: 0,
+              success: function() {
+                return saved.resolve();
+              }
+            });
+            return saved.done(function() {
+              var fetchedLocally;
+              fetchedLocally = $.Deferred();
+              model = new Model({
+                _id: model.id
+              });
+              model.fetch({
+                errorStatus: 0,
+                success: function() {
+                  return fetchedLocally.resolve();
+                }
+              });
+              return fetchedLocally.done(function() {
+                expect(model.get('paper')).to.equal('oragami');
+                return done();
+              });
+            });
+          });
+          return it('updates records created while offline', function(done) {
+            var model, saved;
+            saved = $.Deferred();
+            model = new Model;
+            model.save('paper', 'oragami', {
+              errorStatus: 0,
+              success: function() {
+                return saved.resolve();
+              }
+            });
+            return saved.done(function() {
+              var updated;
+              updated = $.Deferred();
+              model.save('paper', 'mâché', {
+                errorStatus: 0,
+                success: function() {
+                  return updated.resolve();
+                }
+              });
+              return updated.done(function() {
+                var fetchedLocally;
+                fetchedLocally = $.Deferred();
+                model = new Model({
+                  _id: model.id
+                });
+                model.fetch({
+                  errorStatus: 0,
+                  success: function() {
+                    return fetchedLocally.resolve();
+                  }
+                });
+                return fetchedLocally.done(function() {
+                  expect(model.get('paper')).to.equal('mâché');
+                  return done();
+                });
+              });
+            });
+          });
+        });
+        describe('Collection.fetch', function() {
+          return it('loads models that were saved with a common storeName/urlRoot', function(done) {
+            var model1, model2, saved1, saved2;
+            saved1 = $.Deferred();
+            model1 = new Model({
+              a: 1
+            });
+            model1.save('paper', 'oragami', {
+              errorStatus: 0,
+              success: function() {
+                return saved1.resolve();
+              }
+            });
+            saved2 = $.Deferred();
+            model2 = new Model({
+              a: 2
+            });
+            model2.save('paper', 'oragami', {
+              errorStatus: 0,
+              success: function() {
+                return saved2.resolve();
+              }
+            });
+            return $.when(saved1, saved2).done(function() {
+              var collection, fetched;
+              fetched = $.Deferred();
+              collection = new Collection;
+              collection.fetch({
+                errorStatus: 0,
+                success: function() {
+                  return fetched.resolve();
+                }
+              });
+              return fetched.done(function() {
+                expect(collection.size()).to.equal(2);
+                expect(collection.get(model1.id).attributes).to.eql(model1.attributes);
+                expect(collection.get(model2.id).attributes).to.eql(model2.attributes);
+                return done();
+              });
+            });
+          });
+        });
+        return describe('Model.id', function() {
+          return it('obtains a temporary id on new records for use until saved online', function(done) {
+            var model, saved;
+            saved = $.Deferred();
+            model = new Model;
+            model.save(null, {
+              errorStatus: 0,
+              success: function() {
+                return saved.resolve();
+              }
+            });
+            return saved.done(function() {
+              expect(model.id.length).to.equal(36);
+              return done();
+            });
+          });
+        });
+      });
+    });
+    return describe('syncing offline changes when there are dirty or destroyed records', function() {
+      beforeEach(function(done) {
+        var allModified, allSaved;
+        this.collection = new Collection([
+          {
+            _id: 1,
+            name: 'change me'
+          }, {
+            _id: 2,
+            name: 'delete me'
+          }
+        ]);
+        allSaved = this.collection.map(function(model) {
+          var saved;
+          saved = $.Deferred();
+          model.save(null, {
+            success: function() {
+              return saved.resolve();
+            }
+          });
+          return saved;
+        });
+        allModified = $.when.apply($, allSaved).then((function(_this) {
+          return function() {
+            var destroyed, dirtied;
+            dirtied = $.Deferred();
+            _this.collection.get(1).save('name', 'dirty me', {
+              errorStatus: 0,
+              success: function() {
+                return dirtied.resolve();
+              }
+            });
+            destroyed = $.Deferred();
+            _this.collection.get(2).destroy({
+              errorStatus: 0,
+              success: function() {
+                return destroyed.resolve();
+              }
+            });
+            return $.when(dirtied, destroyed);
+          };
+        })(this));
+        return allModified.done(function() {
+          return done();
+        });
+      });
+      describe('Model.fetch', function() {
+        return it('reads models in dirty collections from local storage until a successful sync', function(done) {
+          var fetched, model;
+          fetched = $.Deferred();
+          model = new Model({
+            _id: 1
+          });
+          model.fetch({
+            serverResponse: {
+              _id: 1,
+              name: 'this response is never used'
+            },
+            success: function() {
+              return fetched.resolve();
+            }
+          });
+          return fetched.done(function() {
+            expect(model.get('name')).to.equal('dirty me');
+            return done();
+          });
+        });
+      });
+      describe('Collection.fetch', function() {
+        return it('excludes destroyed models when working locally before a sync', function(done) {
+          var collection, fetched;
+          fetched = $.Deferred();
+          collection = new Collection;
+          collection.fetch({
+            serverResponse: [
+              {
+                _id: 3,
+                name: 'this response is never used'
+              }
+            ],
+            success: function() {
+              return fetched.resolve();
+            }
+          });
+          return fetched.done(function() {
+            expect(collection.size()).to.equal(1);
+            expect(collection.first().get('name')).to.equal('dirty me');
+            return done();
+          });
+        });
+      });
+      describe('Collection.dirtyModels', function() {
+        return it('returns an array of models that have been created or updated while offline', function() {
+          return expect(this.collection.dirtyModels()).to.eql([this.collection.get(1)]);
+        });
+      });
+      describe('Collection.destroyedModelIds', function() {
+        return it('returns an array of ids for models that have been destroyed while offline', function() {
+          return expect(this.collection.destroyedModelIds()).to.eql(['2']);
+        });
+      });
+      describe('Collection.syncDirty', function() {
+        return it('attempts to save online all records that were created/updated while offline', function() {
+          backboneSync.reset();
+          this.collection.syncDirty();
+          expect(backboneSync.callCount).to.equal(1);
+          return expect(this.collection.dirtyModels()).to.eql([]);
+        });
+      });
+      describe('Collection.syncDestroyed', function() {
+        return it('attempts to destroy online all records that were destroyed while offline', function() {
+          backboneSync.reset();
+          this.collection.syncDestroyed();
+          expect(backboneSync.callCount).to.equal(1);
+          return expect(this.collection.destroyedModelIds()).to.eql([]);
+        });
+      });
+      describe('Collection.syncDirtyAndDestroyed', function() {
+        return it('attempts to sync online all records that were modified while offline', function() {
+          backboneSync.reset();
+          this.collection.syncDirtyAndDestroyed();
+          expect(backboneSync.callCount).to.equal(2);
+          expect(this.collection.dirtyModels()).to.eql([]);
+          return expect(this.collection.destroyedModelIds()).to.eql([]);
+        });
+      });
+      return describe('Model.id', function() {
+        return it('for new records with a temporary id is replaced by the id returned by the server', function(done) {
+          var model, saved;
+          saved = $.Deferred();
+          model = new Model;
+          this.collection.add(model);
+          model.save('name', 'created while offline', {
+            errorStatus: 0,
+            success: function() {
+              return saved.resolve();
+            }
+          });
+          return saved.done((function(_this) {
+            return function() {
+              expect(model.id.length).to.equal(36);
+              backboneSync.reset();
+              _this.collection.syncDirty();
+              expect(backboneSync.callCount).to.equal(2);
+              expect(backboneSync.lastCall.args[0]).to.equal('create');
+              expect(backboneSync.lastCall.args[1].id).to.be["null"];
+              expect(backboneSync.lastCall.args[1].get('_id')).to.be["null"];
+              return done();
+            };
+          })(this));
         });
       });
     });
